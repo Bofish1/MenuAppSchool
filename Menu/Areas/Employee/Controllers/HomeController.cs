0368121df4e49bfac3dacc1d9c98247369d61062
unknown <Bofish1>
2022-03-24 16:37:08 -0500

    second Commit

---
+++ Menu/Areas/Employee/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Menu.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Menu.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class HomeController : Controller
+    {
+        private MenuContext context { get; set; }
+
+        public HomeController(MenuContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var foods = context.Foods.Include(m => m.category).OrderBy(m => m.Name).ToList();
+            return View(foods);
+        }
+    }
+}
